45da351eb602d3e4da4c08519516bd4e49dc674a
?ngel ?lvarez <angel@nan-tic.com>
2013-11-28 23:26:05 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,11 @@
+#The COPYRIGHT file at the top level of this repository contains the full
+#copyright notices and license terms.
+
+from trytond.pool import Pool
+from .task import *
+
+def register():
+    Pool.register(
+        WorkType,
+        Work,
+        module='task_tracker', type_='model')
---
+++ task.py
@@ -0,0 +1,25 @@
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+from trytond.model import ModelView, ModelSQL, fields
+from trytond.pool import PoolMeta
+from trytond.pyson import Eval
+
+__all__ = ['WorkType', 'Work']
+__metaclass__ = PoolMeta
+
+
+class WorkType(ModelSQL, ModelView):
+    'Task Tracker'
+    __name__ = 'project.work.tracker'
+
+    name = fields.Char('Name', required=True, select=True)
+    comment = fields.Text('comment')
+
+class Work:
+    __name__ = 'project.work'
+
+    tracker = fields.Many2One('project.work.tracker', 'Tracker',
+        states={
+            'required': Eval('type') == 'task',
+            'invisible': Eval('type') != 'task',
+            }, depends=['type'])
