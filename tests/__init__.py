3500f755dbad8c167580cae7a9c47f63a804c247
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-06-03 16:26:03 +0200

    Increase version to 3.2 and cleanup test

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 
-from .test_project_tracker import suite
+from .test_task_tracker import suite
